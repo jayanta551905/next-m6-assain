5d72c8466f14306b528f3b4f46fd7814516cae15
Jayanta Mondal <jayanta551905@gmail.com>
2023-07-10 00:26:27 +0600

    initital commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+           <h1 className="text-red-600">This is from Header component</h1> 
+        </div>
+    );
+};
+
+export default Header;
